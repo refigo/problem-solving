7661a74b2cf5dbbfe34e34c61a12ee1006a784c1
Mijong Go <42.4.mgo@gmail.com>
2023-06-26 22:48:36 +0900

    [unrated] Title: 배열 만들기 4, Time: 4.71 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    stk = []
+    i = 0
+    while i < len(arr):
+        if (len(stk) > 0) and (stk[-1] >= arr[i]):
+            stk.pop()
+            continue
+        stk.append(arr[i])
+        i += 1
+    return stk
