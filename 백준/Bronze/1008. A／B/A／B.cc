9589a342c5365a69fdcc87aa931aebbb9d3cf150
Mijong Go <42.4.mgo@gmail.com>
2023-08-22 23:56:33 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b;
+int main(){
+	cin >> a >> b;
+	cout << setprecision(numeric_limits<double>::digits10 + 1);
+	cout << (double)((double)a / (double)b) << '\n';
+	return 0;
+}
