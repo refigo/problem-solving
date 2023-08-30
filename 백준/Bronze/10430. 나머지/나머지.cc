ad7c40ad8cba16cb7cd5fd33f7e92cc7f853625a
Mijong Go <42.4.mgo@gmail.com>
2023-08-30 23:55:46 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int A, B, C;
+int main(){
+	cin >> A >> B >> C;
+	cout << (A+B)%C << '\n';
+	cout << ((A%C) + (B%C))%C << '\n';
+	cout << (A*B)%C << '\n';
+	cout << ((A%C) * (B%C))%C << '\n';
+	return 0;
+}
