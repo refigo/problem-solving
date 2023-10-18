190d7ce1815c4699830f2a3ff6a52f94ec89b4d5
Mijong Go <42.4.mgo@gmail.com>
2023-10-18 23:49:53 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+string str;
+int i;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> str;
+	cin >> i;
+	cout << str[i - 1] << '\n';
+	return 0;
+}
