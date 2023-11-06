8cbe04627ece2af615f5d8634822dfa781ca6261
Mijong Go <42.4.mgo@gmail.com>
2023-11-06 19:42:09 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> n;
+	for (int i = 1; i <= n; ++i) {
+		cout << i << '\n';
+	}
+	return 0;
+}
