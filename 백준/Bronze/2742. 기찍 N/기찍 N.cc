42967a9e0f90ae9c8117e6a9525661d636853f8e
Mijong Go <42.4.mgo@gmail.com>
2023-11-09 23:43:50 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> n;
+	do {
+		cout << n << "\n";
+	} while (--n);
+	return 0;
+}
