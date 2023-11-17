294c568407db954ec758674bbc24d64b50878b14
Mijong Go <42.4.mgo@gmail.com>
2023-11-17 23:37:54 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int count_ws, count_star;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		count_ws = i;
+		while (count_ws--) {
+			cout << ' ';
+		}
+		count_star = 2 * (n - i - 1) + 1;
+		while (count_star--) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
