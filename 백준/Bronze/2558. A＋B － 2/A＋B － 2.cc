b49ee04cb15b3d5be5ecf85d96d34d2317587486
Mijong Go <42.4.mgo@gmail.com>
2023-11-30 23:56:25 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> a >> b;
+	cout << a + b << "\n";
+	return 0;
+}
