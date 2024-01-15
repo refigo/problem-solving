7ac958a674d49fdecf3ef7b06a4ac094e3eab345
Mijong Go <42.4.mgo@gmail.com>
2024-01-16 04:33:13 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> s;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" << s << "::fan:\n";
+	cout << ":fan::fan::fan:\n";
+	return 0;
+}
