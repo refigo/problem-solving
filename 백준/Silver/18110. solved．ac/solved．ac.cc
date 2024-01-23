0e9798c6f4411587cdf82bf6796418e8305b106b
Mijong Go <42.4.mgo@gmail.com>
2024-01-23 22:13:08 +0900

    [Silver IV] Title: solved.ac, Time: 48 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.cc
+++ 백준/Silver/18110. solved．ac/solved．ac.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, trim, num, cnt, sum;
+int n, trim, num, sum;
 map<int, int> mp;
 priority_queue<int> pq;
 int main() {
@@ -11,12 +11,9 @@ int main() {
 		cout << 0 << '\n';
 		return 0;
 	}
-	// cout << (float)n << '\n';
-	// cout << float(15) / float(100) << '\n';
 	trim = round((float)(n) * float(15) / float(100));
 	for (int i = 0; i < n; i++) {
 		cin >> num;
-		// mp[num]++;
 		pq.push(num);
 	}
 	for (int i = 0; i < n; i++) {
@@ -25,32 +22,6 @@ int main() {
 		}
 		pq.pop();
 	}
-	// cout << sum << '\n';
-	// cout << (n - (2 * trim)) << '\n';
 	cout << round(float(sum) / float(n - (2 * trim))) << '\n';
-	// for (auto each : pq) {
-	// 	cout << each << '\n';
-	// }
-	// int tmp = 0;
-	// int sum = 0;
-	// for (auto each : mp) {
-	// 	tmp = each.second;
-	// 	if (cnt + tmp <= trim || cnt >= n - trim) {
-	// 		cnt += tmp;
-	// 	} else {
-	// 		cnt > trim -= 
-	// 		if (cnt < trim) {
-	// 			sum += each.first * (cnt + tmp - trim);
-	// 		} else {
-	// 			sum += each.first * tmp;
-	// 		}
-	// 		cnt += tmp;
-	// 	}
-	// 	// if (cnt + tmp > trim && cnt + tmp < n - trim) {
-			
-	// 	// }
-	// }
-	// cout << sum << '\n';
-	// cout << (float(sum) / float(n - (2 * trim))) << '\n';
 	return 0;
 }
