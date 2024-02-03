bc3b586fa2d32c4a1f071251bd60513ff8b231fe
Mijong Go <42.4.mgo@gmail.com>
2024-02-04 05:28:54 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long n, m;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> n >> m;
+	cout << abs(n - m) << '\n';
+	return 0;
+}
