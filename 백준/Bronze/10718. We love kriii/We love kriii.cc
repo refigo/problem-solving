5b959e768dc8ca65251558ca0f44dd70c7fd2a3f
Mijong Go <42.4.mgo@gmail.com>
2024-03-17 04:31:46 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	for (int i = 0; i < 2; i++) {
+		cout << "강한친구 대한육군\n";
+	}
+	return 0;
+}
