723504acdcf75feffb6eda4dbd0df804ece1816c
Mijong Go <42.4.mgo@gmail.com>
2024-03-18 19:14:23 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, k;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> k >> k;
+	}
+	cout << "비와이\n";
+	return 0;
+}
