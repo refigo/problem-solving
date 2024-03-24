663ac845737680e63b476a27d9aa78f19d5bd570
Mijong Go <42.4.mgo@gmail.com>
2024-03-24 19:00:45 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int r1, s;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> r1 >> s;
+	cout << 2 * s - r1 << '\n';
+	return 0;
+}
