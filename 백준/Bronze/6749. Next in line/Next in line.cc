39b90d50e7334fec5668083fdd849ffd7e46040f
Mijong Go <42.4.mgo@gmail.com>
2024-03-27 00:00:47 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int y, m;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> y;
+	cin >> m;
+	cout << m + (m - y) << '\n';
+	return 0;
+}
