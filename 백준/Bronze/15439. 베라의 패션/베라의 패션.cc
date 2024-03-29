3ad64bb34ca7b572f83eaab150bb88166c760787
Mijong Go <42.4.mgo@gmail.com>
2024-03-29 23:56:05 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> n;
+	cout << n * (n - 1) << '\n';
+	return 0;
+}
