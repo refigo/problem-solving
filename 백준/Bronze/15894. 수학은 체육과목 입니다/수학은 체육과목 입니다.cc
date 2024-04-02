77e68e3d50af6f16cdfb7bc25332b99f8db03f01
Mijong Go <42.4.mgo@gmail.com>
2024-04-02 23:59:14 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long n;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> n;
+	cout << n * 4 << '\n';
+	return 0;
+}
