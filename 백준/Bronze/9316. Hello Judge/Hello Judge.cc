374f682cabb494c9e212e9051f414c30e5fe2036
Mijong Go <42.4.mgo@gmail.com>
2024-04-28 18:35:48 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cout << "Hello World, Judge " << i << "!\n";
+		
+	}
+	return 0;
+}
