fa45931fe081ef6f72f14f17656fe15034a53ab5
Mijong Go <42.4.mgo@gmail.com>
2024-05-16 18:15:32 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int s, t, d;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> s >> t >> d;
+	cout << d / (s * 2) * t << endl;
+	return 0;
+}
