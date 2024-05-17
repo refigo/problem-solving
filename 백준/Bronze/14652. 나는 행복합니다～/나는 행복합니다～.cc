7ed1f2fc12be02bd28b8bc9f42db9dfe89ce77dd
Mijong Go <42.4.mgo@gmail.com>
2024-05-17 15:31:30 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, k;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> n >> m >> k;
+	cout << k / m << ' ' << k % m << '\n';
+}
