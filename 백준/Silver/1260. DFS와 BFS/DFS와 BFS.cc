35a40a22d8f525a7d8ca2529d05db14fe73526d4
Mijong Go <42.4.mgo@gmail.com>
2024-05-30 16:29:54 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,39 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, v, a, b;
-int visited[1004];
-set<int> adj[1004];
+
+int n, m, v;
+int a, b;
+vector<int> adj[1004];
+bool visited[1004];
+
 void dfs(int here) {
-	visited[here] = 1;
+	visited[here] = true;
 	cout << here << ' ';
 	for (int there : adj[here]) {
 		if (visited[there]) continue;
 		dfs(there);
 	}
 }
+
 void bfs(int start) {
-	queue<int> q;
-	q.push(start);
-	visited[start] = 1;
-	while (q.size()) {
-		int here = q.front();
-		q.pop();
+	queue<int> que;
+	visited[start] = true;
+	que.push(start);
+	int here = 0;
+	while (que.size()) {
+		here = que.front();
+		que.pop();
 		cout << here << ' ';
 		for (int there : adj[here]) {
 			if (visited[there]) continue;
-			q.push(there);
-			visited[there] = 1;
+			que.push(there);
+			visited[there] = true;
 		}
 	}
 }
+
 int main() {
 	cin.tie(0); cout.tie(0);
 	ios::sync_with_stdio(false);
 	cin >> n >> m >> v;
 	while (m--) {
 		cin >> a >> b;
-		adj[a].insert(b);
-		adj[b].insert(a);
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+	for (int i = 1; i <= n; i++) {
+		sort(adj[i].begin(), adj[i].end());
 	}
 	dfs(v);
 	cout << '\n';
