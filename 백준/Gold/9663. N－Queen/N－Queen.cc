3dfade3f97360ac647151aa1b233276477412225
Mijong Go <42.4.mgo@gmail.com>
2024-05-30 15:06:55 +0900

    [Gold IV] Title: N-Queen, Time: 4412 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,33 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int n;
 int a[19];
-bool verify(int y) {
+
+bool is_valid(int y) {
 	for (int i = y - 1; i >= 0; i--) {
 		if (a[y] == a[i] 
-		|| abs(y - i) == abs(a[y] - a[i])) {
+		|| ((y - i) == abs(a[y] - a[i]))) {
 			return false;
 		}
 	}
 	return true;
 }
-int queen(int y){
+
+int n_queen(int y) {
+	int ret = 0;
+	// termination condition
 	if (y == n) {
 		return 1;
 	}
-	int ret = 0;
 	for (int x = 0; x < n; x++) {
 		a[y] = x;
-		if (verify(y)) {
-			ret += queen(y + 1);
+		if (is_valid(y)) {
+			ret += n_queen(y + 1);
 		}
 	}
 	return ret;
 }
+
 int main() {
 	cin.tie(0); cout.tie(0);
 	ios::sync_with_stdio(false);
 	cin >> n;
-	cout << queen(0) << '\n';
+	cout << n_queen(0);
 	return 0;
 }
