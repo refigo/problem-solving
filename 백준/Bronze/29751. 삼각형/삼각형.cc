ed741a414498e85a72b56798376289b24d041aeb
Mijong Go <42.4.mgo@gmail.com>
2024-06-02 23:58:29 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+float w, h, a;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> w >> h;
+	a = w * h / 2;
+	cout << fixed << setprecision(1) << a << endl;
+	return 0;
+}
