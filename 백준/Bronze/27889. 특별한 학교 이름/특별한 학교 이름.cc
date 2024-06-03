055bfc2399b0288a99865a963c9135a08b957152
Mijong Go <42.4.mgo@gmail.com>
2024-06-03 18:58:18 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> s;
+	if (s == "NLCS") {
+		cout << "North London Collegiate School";
+	}
+	if (s == "BHA") {
+		cout << "Branksome Hall Asia";
+	}
+	if (s == "KIS") {
+		cout << "Korea International School";
+	}
+	if (s == "SJA") {
+		cout << "St. Johnsbury Academy";
+	}
+	return 0;
+}
