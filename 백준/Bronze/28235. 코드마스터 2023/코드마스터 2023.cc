0518d37b049da346a1abf698ea37442356e3801c
Mijong Go <42.4.mgo@gmail.com>
2024-06-07 23:59:46 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int main() {
+	cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+	cin >> s;
+	if (s == "SONGDO") {
+		cout << "HIGHSCHOOL";
+	} else if (s == "CODE") {
+		cout << "MASTER";
+	} else if (s == "2023") {
+		cout << "0611";
+	} else if (s == "ALGORITHM") {
+		cout << "CONTEST";
+	}
+	return 0;
+}
