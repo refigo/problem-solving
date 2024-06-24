0d15c75d293c0abad804a6752d0053d254028bee
Mijong Go <42.4.mgo@gmail.com>
2024-06-24 21:09:07 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int d1, d2;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin >> d1 >> d2;
+	cout << fixed << setprecision(6);
+	cout << (float)((float)d1 * (float)2) + (float)(3.141592 * (float)d2 * (float)2);
+	return 0;
+}
