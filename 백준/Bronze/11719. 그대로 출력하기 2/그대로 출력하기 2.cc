d7250215a89bd688a92504a6981872671fe6280a
Mijong Go <42.4.mgo@gmail.com>
2024-07-01 22:34:21 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0);
+	while (getline(cin, s)) {
+		cout << s << '\n';
+		// eof
+		if (cin.eof()) break;
+	}
+	return 0;
+}
