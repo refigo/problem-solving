625bd590b3abd4d570ec263cbcb59d834636c41d
Mijong Go <42.4.mgo@gmail.com>
2024-07-08 20:05:09 +0900

    [Bronze III] Title: 저작권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, i;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin >> a >> i;
+	cout << a * (i - 1) + 1;
+	return 0;
+}
