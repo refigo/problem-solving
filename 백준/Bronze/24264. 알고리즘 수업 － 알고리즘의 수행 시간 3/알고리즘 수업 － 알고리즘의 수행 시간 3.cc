f43a4275b6db718ccc17d4e1a411689296dd05ac
Mijong Go <42.4.mgo@gmail.com>
2024-07-19 20:35:01 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long n_menofpassion2;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin >> n_menofpassion2;
+	cout << n_menofpassion2 * n_menofpassion2 << '\n';
+	cout << 2 << '\n';
+	return 0;
+}
