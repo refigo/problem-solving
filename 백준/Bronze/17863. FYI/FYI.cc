77df9fd98b9ae3f2a21edd25d65125431ec43157
Mijong Go <42.4.mgo@gmail.com>
2024-08-04 23:51:16 +0900

    [Bronze IV] Title: FYI, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin >> n;
+	if (n / 10000 == 555) cout << "YES";
+	else cout << "NO";
+	return 0;
+}
