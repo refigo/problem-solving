867a1312926b2d23e6bf37d3fefc9aaf98ddded1
Mijong Go <42.4.mgo@gmail.com>
2024-09-26 18:47:54 +0900

    [Bronze IV] Title: Archivist, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int year;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin >> year;
+	if (year == 1995) cout << "ITMO";
+	else if (year == 1996) cout << "SPbSU";
+	else if (year == 1997) cout << "SPbSU";
+	else if (year == 1998) cout << "ITMO";
+	else if (year == 1999) cout << "ITMO";
+	else if (year == 2000) cout << "SPbSU";
+	else if (year == 2001) cout << "ITMO";
+	else if (year == 2002) cout << "ITMO";
+	else if (year == 2003) cout << "ITMO";
+	else if (year == 2004) cout << "ITMO";
+	else if (year == 2005) cout << "ITMO";
+	else if (year == 2006) cout << "PetrSU, ITMO";
+	else if (year == 2007) cout << "SPbSU";
+	else if (year == 2008) cout << "SPbSU";
+	else if (year == 2009) cout << "ITMO";
+	else if (year == 2010) cout << "ITMO";
+	else if (year == 2011) cout << "ITMO";
+	else if (year == 2012) cout << "ITMO";
+	else if (year == 2013) cout << "SPbSU";
+	else if (year == 2014) cout << "ITMO";
+	else if (year == 2015) cout << "ITMO";
+	else if (year == 2016) cout << "ITMO";
+	else if (year == 2017) cout << "ITMO";
+	else if (year == 2018) cout << "SPbSU";
+	else if (year == 2019) cout << "ITMO";
+	return 0;
+}
