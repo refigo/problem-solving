4bdc3a62d742e008f0aab43eff8d01dc8f00b265
Mijong Go <42.4.mgo@gmail.com>
2024-11-12 19:44:03 +0900

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, k;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin >> n >> m >> k;
+	cout << (m / n) * k << '\n';
+	return 0;
+}
