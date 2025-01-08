4af17e2bfd9a4b17db5ac38bb440703f93495de4
Mijong Go <42.4.mgo@gmail.com>
2025-01-08 20:39:22 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin >> n;
+	while (n--) {
+		cout << 'a';
+	}
+	return 0;
+}
