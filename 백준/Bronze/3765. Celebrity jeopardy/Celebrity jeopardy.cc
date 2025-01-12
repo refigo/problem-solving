8b7e0d3f66965a6e6d8340f21f5abf30887de044
Mijong Go <42.4.mgo@gmail.com>
2025-01-12 19:06:42 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0);
+	while (getline(cin, s)) {
+		cout << s << '\n';
+	}
+	return 0;
+}
