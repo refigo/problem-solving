6ee88fb67a30395ade95f36e3457e9788979ca4a
Mijong Go <42.4.mgo@gmail.com>
2025-02-22 11:36:48 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 10007;
+const int MAX_N = 1001;
+int dp[MAX_N];
+
+void precompute() {
+    dp[1] = 1;
+    dp[2] = 3;
+    
+    for (int i = 3; i < MAX_N; i++) {
+        dp[i] = (dp[i-1] + dp[i-2] * 2) % MOD;
+    }
+}
+
+int main() {
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);
+    
+    precompute();
+    
+    int n;
+    cin >> n;
+    cout << dp[n] << '\n';
+    
+    return 0;
+}
