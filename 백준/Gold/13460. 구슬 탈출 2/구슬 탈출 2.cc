56f66c97a3157b83feac898a2a34de79f4e86405
Mijong Go <42.4.mgo@gmail.com>
2025-05-05 23:55:16 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.cc
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.cc
@@ -3,22 +3,19 @@ using namespace std;
 
 int n, m;
 vector<string> board;
-int dx[4] = {-1, 1, 0, 0}; // 상, 하, 좌, 우
+int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
-// 구슬의 위치와 이동 횟수를 저장하는 구조체
 struct State {
-    int rx, ry; // 빨간 구슬 위치
-    int bx, by; // 파란 구슬 위치
-    int count;  // 이동 횟수
+    int rx, ry;
+    int bx, by;
+    int count;
 };
 
-// 구슬을 특정 방향으로 기울일 때 이동 결과 계산
 pair<int, int> move(int x, int y, int dir, int& distance) {
     int nx = x, ny = y;
     distance = 0;
     
-    // 벽을 만나거나 구멍에 빠질 때까지 이동
     while (board[nx + dx[dir]][ny + dy[dir]] != '#' && board[nx][ny] != 'O') {
         nx += dx[dir];
         ny += dy[dir];
@@ -29,7 +26,6 @@ pair<int, int> move(int x, int y, int dir, int& distance) {
 }
 
 int bfs() {
-    // 빨간 구슬과 파란 구슬의 초기 위치 찾기
     int rx = -1, ry = -1, bx = -1, by = -1;
     int holeX = -1, holeY = -1;
     
@@ -37,11 +33,11 @@ int bfs() {
         for (int j = 0; j < m; j++) {
             if (board[i][j] == 'R') {
                 rx = i; ry = j;
-                board[i][j] = '.'; // 구슬 위치를 빈 칸으로 표시
+                board[i][j] = '.';
             }
             if (board[i][j] == 'B') {
                 bx = i; by = j;
-                board[i][j] = '.'; // 구슬 위치를 빈 칸으로 표시
+                board[i][j] = '.';
             }
             if (board[i][j] == 'O') {
                 holeX = i; holeY = j;
@@ -49,11 +45,9 @@ int bfs() {
         }
     }
     
-    // BFS 시작
     queue<State> q;
     q.push({rx, ry, bx, by, 0});
     
-    // 방문 상태 체크를 위한 맵
     map<tuple<int, int, int, int>, bool> visited;
     visited[{rx, ry, bx, by}] = true;
     
@@ -61,30 +55,24 @@ int bfs() {
         State cur = q.front();
         q.pop();
         
-        // 현재 횟수가 10번을 초과하면 중단
         if (cur.count >= 10) {
             continue;
         }
         
-        // 현재 위치에서 구슬을 4방향으로 기울이기
         for (int dir = 0; dir < 4; dir++) {
             int redDist = 0, blueDist = 0;
             
-            // 빨간 구슬과 파란 구슬 이동
             auto [nrx, nry] = move(cur.rx, cur.ry, dir, redDist);
             auto [nbx, nby] = move(cur.bx, cur.by, dir, blueDist);
             
-            // 파란 구슬이 구멍에 빠진 경우 (실패 케이스) - 다음 방향 시도
             if (board[nbx][nby] == 'O') {
                 continue;
             }
             
-            // 빨간 구슬이 구멍에 빠진 경우 (성공)
             if (board[nrx][nry] == 'O') {
                 return cur.count + 1;
             }
             
-            // 두 구슬이 같은 위치에 있는 경우, 이동 거리가 더 긴 구슬을 한 칸 뒤로 조정
             if (nrx == nbx && nry == nby) {
                 if (redDist > blueDist) {
                     nrx -= dx[dir];
@@ -95,7 +83,6 @@ int bfs() {
                 }
             }
             
-            // 새로운 상태가 아직 방문하지 않았다면 큐에 추가
             if (!visited[{nrx, nry, nbx, nby}]) {
                 visited[{nrx, nry, nbx, nby}] = true;
                 q.push({nrx, nry, nbx, nby, cur.count + 1});
@@ -103,7 +90,7 @@ int bfs() {
         }
     }
     
-    return -1; // 10번 이하로 빨간 구슬을 구멍에 넣을 수 없는 경우
+    return -1;
 }
 
 int main() {
