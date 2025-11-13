3adbe93dfb2d6fb3a3961d72f98c60c812207bab
Mijong Go <42.4.mgo@gmail.com>
2025-11-13 12:37:13 +0900

    [Bronze III] Title: Изгороди, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24623. Изгороди/Изгороди.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, alpha;
+    if (!(cin >> n)) return 0;
+    if (!(cin >> alpha)) return 0;
+    cout << 90 << '\n';
+    return 0;
+}
