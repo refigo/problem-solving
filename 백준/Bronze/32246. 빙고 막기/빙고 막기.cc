8d1901fda15dc0b1cb2b6e395fde1ea771cc3bd4
Mijong Go <42.4.mgo@gmail.com>
2025-12-05 16:25:25 +0900

    [Bronze III] Title: 빙고 막기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32246. 빙고 막기/빙고 막기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N; 
+    if(!(cin >> N)) return 0;
+    if (N == 2) cout << 3 << '\n';
+    else cout << N << '\n';
+    return 0;
+}
